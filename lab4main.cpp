4e43006c7cab12a18a7d1a98ccaf4627dff3e48d
Joseph Schell <alejandro1one@hotmail.com>
2016-12-28 00:52:45 -0800

    Update lab4main.cpp
    Removed using namespace std. Added system pause upon quitting program.

--- lab4main.cpp
+++ lab4main.cpp
@@ -10,16 +10,17 @@ int main() {
 	Pokedex list;
 	char reply;
 
-	cout << "Welcome to the Pokemon collection program." << endl;
+	std::cout << "Welcome to the Pokemon collection program." << std::endl;
 	list.loadData();
 
 	do {
-		cout << "What would you like to do: (L)ist, (A)dd, (S)earch, "
+		std::cout << "What would you like to do: (L)ist, (A)dd, (S)earch, "
 			<< "find (M)ax hit point creature, or (Q)uit: ";
 		reply = readChoice();
 		list.exeChoice(reply);
 	} while (toupper(reply) != 'Q');
 
+	system("pause");
 	return 0;
 }
 
@@ -27,34 +28,34 @@ int main() {
 char readChoice() {
 	char choice;
 
-	cin >> choice;
-	cin.ignore(100, '\n');
+	std::cin >> choice;
+	std::cin.ignore(100, '\n');
 	return choice;
 }
 
 // Verifies user input is of data type 'int'.
 int readInt(const char prompt[]) {
 	int temp = 0;
-	cout << prompt;
-	cin >> temp;
+	std::cout << prompt;
+	std::cin >> temp;
 
-	while (!cin) //Error Check
+	while (!std::cin) //Error Check
 	{
-		cin.clear();
-		cin.ignore(100, '\n');
-		cout << "Invalid input! Try again!!\n";
-		cin >> temp;
+		std::cin.clear();
+		std::cin.ignore(100, '\n');
+		std::cout << "Invalid input! Try again!!\n";
+		std::cin >> temp;
 	}
-	cin.ignore(100, '\n');
+	std::cin.ignore(100, '\n');
 	return temp;
 }
 
 // Verifies user inputs c-string.
 void readString(char inputStr[], const char prompt[]) {
-	cout << prompt;
-	cin.getline(inputStr, strSize, '\n');
-	while (!cin) {
-		cin.clear();
-		cin.ignore(100, '\n');
+	std::cout << prompt;
+	std::cin.getline(inputStr, strSize, '\n');
+	while (!std::cin) {
+		std::cin.clear();
+		std::cin.ignore(100, '\n');
 	}
 }
